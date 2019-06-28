52d0c31fd162d416bd48ab56c5e4860dd588eae6
WDelenclos <wdelenclos@gmail.com>
2019-06-28 15:25:16 +0200

    [improve] root endpoint

--- api/app.js
+++ api/app.js
@@ -3,19 +3,18 @@ const bodyparser = require('body-parser');
 const db = require('./lib/db');
 const BookRouter = require('./routes/book');
 const UserRouter = require('./routes/user');
-const router =  express.Router();
+const RootRouter = require('./routes/root');
 
 const app = express();
 console.log("\n\n\n\n|-------  PREFACE REST API  -------|");
 
 app.use(bodyparser.json());
 
-router.get('/', (req, res) => {
-   res.sendFile('/static/index.html');
-});
-
+// ROUTES
+app.use('', RootRouter);
 app.use('/books', BookRouter);
 app.use('/users', UserRouter);
+
 app.listen(3003, () => console.log("|--> 📡 HTTP Ready [:3003]"));
 
 db.once('open', () => {});
