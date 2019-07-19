5a4eb4a811d87c095b97d822f5d3815666a260fe
Dylan Settbon <dylans_set@hotmail.fr>
2019-07-19 16:44:27 +0200

    [ADD] AVG Avis

--- api/routes/book.js
+++ api/routes/book.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const Book = require('../models/book');
-const User = require('../models/user')
-const router =  express.Router();
+const User = require('../models/user');
+const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    Book.find(req.query).then(data => res.json(data));
+    Book.find(req.query).populate('avis').then(data => {
+        data.forEach(function(dataEl){
+            let avg = 0;
+            let iter = 0;
+            dataEl.avis.forEach(function (element) {
+                iter++;
+                avg += element.notation;
+            });
+            if(dataEl.notation !== undefined){
+                dataEl.notation.note = avg / iter;
+                dataEl.notation.numberOfReviews = iter;
+            }
+
+        })
+
+        res.json(data)
+    });
 });
 
 // book/1
 router.get("/:id", (req, res) => {
-    Book.findOne({ _id: req.params.id })
-        .then(data => res.json(data))
+    Book.findById(req.params.id).populate('avis')
+        .then(data => {
+            let avg = 0;
+            let iter = 0;
+            data.avis.forEach(function (element) {
+                iter++;
+                avg += element.notation;
+            });
+            data.notation.note = avg / iter;
+            data.notation.numberOfReviews = iter;
+            res.json(data)
+        })
         .catch(error => {
             console.log(error);
             if (error.name == "CastError") {
-                res.status(422).json({ message: "Invalid id" });
+                res.status(422).json({message: "Invalid id"});
             } else {
                 res.sendStatus(500);
             }
@@ -26,7 +52,7 @@ router.get("/:id", (req, res) => {
 router.get("/:id/avis/", (req, res) => {
     const query = Book.findById(req.params.id).populate('avis');
     query.exec((err, book) => {
-        return res.status(200).json({ book, avis: book.avis })
+        return res.status(200).json({book, avis: book.avis})
     })
 });
 
@@ -35,7 +61,7 @@ router.post('/', (req, res) => {
     book.save()
         .then(data => res.status(201).send(data))
         .catch(error => {
-            if(error.name === 'ValidationError'){
+            if (error.name === 'ValidationError') {
                 res.status(400).json(error.errors);
             } else {
                 res.sendStatus(500);
@@ -45,28 +71,30 @@ router.post('/', (req, res) => {
 
 router.post('/finds', (req, res) => {
     Book.find({
-        '_id': { $in: 
+        '_id': {
+            $in:
             req.body
-        }}).then(data => res.status(201).send(data));
+        }
+    }).then(data => res.status(201).send(data));
 });
 
 router.post('/:bookId/add', (req, res) => {
     User.findById(req.body.userId).exec((err, user) => {
-        user.like.push(req.params.bookId)
-        user.save(function(err,data){
-            if(err) return console.log(err.stack);
+        user.like.push(req.params.bookId);
+        user.save(function (err, data) {
+            if (err) return console.log(err.stack);
             res.status(201).json(user)
-          });
+        });
     })
 });
 
 router.post('/:bookId/remove', (req, res) => {
     User.findById(req.body.userId).exec((err, user) => {
-        user.like.pull(req.params.bookId)
-        user.save(function(err,data){
-            if(err) return console.log(err.stack);
+        user.like.pull(req.params.bookId);
+        user.save(function (err, data) {
+            if (err) return console.log(err.stack);
             res.status(201).json(user)
-          });
+        });
     })
 });
 
